edf525560364ce1c64b590b93702dc03b9a0d581
balambigai01 <balambigai6050@gmail.com>
2024-08-13 15:53:11 +0800

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
+import path from 'path'
 import bodyParser from "body-parser"
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -20,7 +21,7 @@ app.use(express.urlencoded({extended:true})) // parse form data
 
 const PORT=process.env.PORT || 5000
 
-
+const __dirname=path.resolve()
 // app.use(express.json({ limit: 50mb' })); 
 app.use(express.urlencoded({ limit: '1mb', extended: true }));// Adjust limit as needed
 // app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -30,6 +31,12 @@ app.use('/api/auth',authRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/posts',postRoutes)
 app.use('/api/notifications',notificationRoutes)
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 app.listen(PORT,()=>{
     console.log(`server is running on the port ${PORT}`)
     connectMongoDB()
